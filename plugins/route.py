d0e557887dcc5b1ca388daca0d2d32aa580c7434
mrzbotbot-dot <mrzbotbot@gmail.com>
2026-03-29 20:56:44 +0530

    Update plugins/route.py with new content

--- plugins/route.py
+++ plugins/route.py
@@ -1,5 +1,11 @@
+from aiohttp import web
+
 def media_streamer():
     from Metrixbotz import some_function  # Lazy import
     # Other code for media_streamer function
-    
-# Additional code...
+    pass
+
+# Define routes list for web server
+routes = [
+    # Add your routes here
+]
